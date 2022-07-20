a51ec4c298face104abdf3f6cb066a70b405df1d
xhUdiF <95344881+xhUdiF@users.noreply.github.com>
2022-07-19 22:08:06 -0700

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,17 +1,7 @@
-const SingleDay = 'bnmlkj-xhudif.cloud.okteto.net'
-const DoubleDay = 'bnmlkj-xhudif.cloud.okteto.net'
 addEventListener(
     "fetch",event => {
-    
-        let nd = new Date();
-        if (nd.getDate()%2) {
-            host = SingleDay
-        } else {
-            host = DoubleDay
-        }
-        
         let url=new URL(event.request.url);
-        url.hostname=host;
+        url.hostname='bnmlkj-xhudif.cloud.okteto.net';
         let request=new Request(url,event.request);
         event. respondWith(
             fetch(request)
